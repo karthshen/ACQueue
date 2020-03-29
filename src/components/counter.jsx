b8dbbe24d25fc912584fa7352b3dc5bc7b0eec42
karthshen <karth-shen@hotmail.com>
2020-03-29 01:49:35 -0400

    Generated button with Style and tags

---
+++ src/components/counter.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+  styles = {
+    fontSize: 20,
+    fontWeight: "bold"
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <span style={this.styles} className={this.getBadgeClasses()}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btm-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const count = this.state.count;
+
+    if (count === 0) {
+      return "Zero";
+    } else {
+      return count;
+    }
+  }
+}
+
+export default Counter;
