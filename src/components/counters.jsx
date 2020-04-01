e66ddc68ed24284efc96aacc1fb74624b0698a30
karthshen <karth-shen@hotmail.com>
2020-04-01 08:19:21 -0400

    align button and form

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -20,10 +20,17 @@ class Counters extends Component {
             ></Counter>
           ))}
         </div>
-        <button
-          onClick={onAdding}
-          className="btn btn-success btn-la m-2"
-        >Add</button>
+        <div className="input-group">
+          <span className="input-group-btn">
+            <button
+              onClick={onAdding}
+              className="btn btn-success btn-la m-2"
+            >Add</button>
+          </span>
+          <div className="col-xs-2 m-2">
+            <input type="text" className="form-control" placeholder="First name"></input>
+          </div>
+        </div>
         <button
           onClick={onReset}
           className="btn btn-warning btn-la m-2"
@@ -33,13 +40,6 @@ class Counters extends Component {
         <button
           onClick={onClear}
           className="btn btn-danger btn-la m-2">Clear</button>
-        <form>
-          <div className="form-row">
-            <div className="col-xs-2 m-2">
-              <input type="text" className="form-control" placeholder="First name"></input>
-            </div>
-          </div>
-        </form>
       </React.Fragment>
     );
   }
