18c1e2cb90daccc64d78612a9bafb7d3494915d7
karthshen <karth-shen@hotmail.com>
2020-04-01 15:24:09 -0400

    Updates

---
+++ precache-manifest.091e1e424bf9e50207c26d0757bbec48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3435c649ade1445e3ebed5f9e1a12c84",
+    "url": "/AnimalCrossingQueue/index.html"
+  },
+  {
+    "revision": "42bb84ff85c924bc0562",
+    "url": "/AnimalCrossingQueue/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "42bb84ff85c924bc0562",
+    "url": "/AnimalCrossingQueue/static/js/2.9db30601.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/AnimalCrossingQueue/static/js/2.9db30601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f4abb518926b9249f5e",
+    "url": "/AnimalCrossingQueue/static/js/main.c0a52d81.chunk.js"
+  },
+  {
+    "revision": "449065d599758ca9c67b",
+    "url": "/AnimalCrossingQueue/static/js/runtime-main.fc0813ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AnimalCrossingQueue/static/media/logo.5d5d9eef.svg"
+  }
+]);
