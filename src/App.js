49db2a42f2b848cad857eb3e2d214d986ef9a74d
karthshen <karth-shen@hotmail.com>
2020-04-01 07:03:43 -0400

    Remove css for app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+//import "./App.css";
 import NavBar from './components/navbar'
 import Counters from "./components/counters";
 
